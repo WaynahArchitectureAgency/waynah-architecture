a1a23c90258073c32fc83e2a30b0a85c1052832d
Israil Doukhaev <israilst67@gmail.com>
2022-06-30 19:46:03 +0200

    change href of menu

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -32,23 +32,23 @@ const Menu = ({designOn}: {designOn?: string}) => {
         <div className="row menuGeneral">
             <div className="col-12 menu">
                 <a href="/" className="textBlack"><AiOutlineHome className="homeIcon"/></a>
-                <a href='project' className={designOn === 'projects' ? ('textBlackWithDecoration') : ('textBlack')}>Projects</a>
-                <a href='research'className={designOn === 'research' ? ('textBlackWithDecoration') : ('textBlack')}>Research</a>
-                <a href='team' className={designOn === 'team' ? ('textBlackWithDecoration') : ('textBlack')}>Team</a>
-                <a href='news' className={designOn === 'news' ? ('textBlackWithDecoration') : ('textBlack')}>News</a>
-                <a href='mypost' className={designOn === 'myPosts' ? ('textBlackWithDecoration') : ('textBlack')}>My Posts</a>
-                <a href='createPost' className={designOn === 'createPost' ? ('textBlackWithDecoration') : ('textBlack')}>Create Post</a>
-                <a href='profile' className={designOn === 'profile' ? ('textBlackWithDecoration') : ('textBlack')}>Profile</a>
+                <a href='/project' className={designOn === 'projects' ? ('textBlackWithDecoration') : ('textBlack')}>Projects</a>
+                <a href='/research'className={designOn === 'research' ? ('textBlackWithDecoration') : ('textBlack')}>Research</a>
+                <a href='/team' className={designOn === 'team' ? ('textBlackWithDecoration') : ('textBlack')}>Team</a>
+                <a href='/news' className={designOn === 'news' ? ('textBlackWithDecoration') : ('textBlack')}>News</a>
+                <a href='/mypost' className={designOn === 'myPosts' ? ('textBlackWithDecoration') : ('textBlack')}>My Posts</a>
+                <a href='/createPost' className={designOn === 'createPost' ? ('textBlackWithDecoration') : ('textBlack')}>Create Post</a>
+                <a href='/profile' className={designOn === 'profile' ? ('textBlackWithDecoration') : ('textBlack')}>Profile</a>
             </div>
         </div>
     ) :(
         <div className="row menuGeneral">
             <div className="col-12 menu">
                 <a href="/" className="textBlack"><AiOutlineHome className="homeIcon"/></a>
-                <a href='project' className={designOn === 'projects' ? ('textBlackWithDecoration') : ('textBlack')}>Projects</a>
-                <a href='research'className={designOn === 'research' ? ('textBlackWithDecoration') : ('textBlack')}>Research</a>
-                <a href='team' className={designOn === 'team' ? ('textBlackWithDecoration') : ('textBlack')}>Team</a>
-                <a href='news' className={designOn === 'news' ? ('textBlackWithDecoration') : ('textBlack')}>News</a>
+                <a href='/project' className={designOn === 'projects' ? ('textBlackWithDecoration') : ('textBlack')}>Projects</a>
+                <a href='/research'className={designOn === 'research' ? ('textBlackWithDecoration') : ('textBlack')}>Research</a>
+                <a href='/team' className={designOn === 'team' ? ('textBlackWithDecoration') : ('textBlack')}>Team</a>
+                <a href='/news' className={designOn === 'news' ? ('textBlackWithDecoration') : ('textBlack')}>News</a>
             </div>
         </div>
     )  
